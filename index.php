d8188ba663791b82cf4e4823c12ed78c1e15977d
msilvae <34558233+msilvae@users.noreply.github.com>
2020-10-29 14:34:25 -0300

    alteracao 1

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
 <?php $i = (9+1); ?>
-<h1>Bem vindo turma <?php echo "$i B"; ?> com professor Danilo Aparecido</h1>
+<h1>Bem vindo turma <?php echo "$i A"; ?> com professor Danilo Aparecido</h1>
+<br>
+<h1>Aluno Edson Fernandes</h1>
